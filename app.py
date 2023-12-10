bf47979314c601148b94c063a4a832953ebb659e
afraa-n <143756865+afraa-n@users.noreply.github.com>
2023-12-10 20:08:53 +0000

    app

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, request, render_template
 from dotenv import load_dotenv
 import os
 import openai
+import re
 
 app = Flask(__name__)
 load_dotenv()
@@ -20,14 +21,14 @@ def recommend():
     user_input = request.form['user_input']
 
     # Use OpenAI API for movie recommendation
-    recommendations = get_movie_recommendations(user_input, num_recommendations=10)
+    recommendations = get_movie_recommendations(user_input, num_recommendations=1)
 
-    return render_template('recommendation.html', recommendations=recommendations)
+    return render_template('recommendation.html', recommendation=recommendations[0])
 
 def get_movie_recommendations(user_input, num_recommendations=1):
     # Use OpenAI API to generate movie recommendations based on user input
     separator = "><"
-    prompt = f"Recommend me 10 unique movies that are similar to this theme/genre: {user_input}. Give me a structured output by providing details for each movie in the following format:\n1. Title:\n2. Genre:\n3. Release Year:\n4. Director:\n5. Description:{separator}"
+    prompt = f"Recommend me a movie that is similar to this theme/genre: {user_input}. Provide details in the following format:\nTitle:\nGenre:\nRelease Year:\nDirector:\nDescription:{separator}. I want to put this in a tabular format so follow this format always."
     response = openai.Completion.create(
         model="text-davinci-003",
         prompt=prompt,
@@ -36,42 +37,20 @@ def get_movie_recommendations(user_input, num_recommendations=1):
 
     recommendations_text = response['choices'][0]['text']
 
-    # Split the recommendations using the separator
-    recommendations_list = recommendations_text.split(separator)
+    # Use regex to extract movie details
+    matches = re.findall(r"Title:(.+?)\s+Genre:(.+?)\s+Release Year:(.+?)\s+Director:(.+?)\s+Description:(.+?)(?=\nTitle:|$)", recommendations_text, re.DOTALL)
 
     recommendations = []
 
-    for i in range(num_recommendations):
-        if i < len(recommendations_list):
-            recommendation_text = recommendations_list[i].strip()
-            title_start = recommendation_text.find(f"{i + 1}. Title:")
-            genre_start = recommendation_text.find(f"{i + 1}. Genre:")
-            release_year_start = recommendation_text.find(f"{i + 1}. Release Year:")
-            director_start = recommendation_text.find(f"{i + 1}. Director:")
-            description_start = recommendation_text.find(f"{i + 1}. Description:")
-
-            title = recommendation_text[title_start + len(f"{i + 1}. Title:"):genre_start].strip()
-            genre = recommendation_text[genre_start + len(f"{i + 1}. Genre:"):release_year_start].strip()
-            release_year = recommendation_text[release_year_start + len(f"{i + 1}. Release Year:"):director_start].strip()
-            director = recommendation_text[director_start + len(f"{i + 1}. Director:"):description_start].strip()
-            description = recommendation_text[description_start + len(f"{i + 1}. Description:"):].strip()
-
-            recommendations.append({
-                'title': title,
-                'genre': genre,
-                'release_year': release_year,
-                'director': director,
-                'description': description
-            })
-        else:
-            # Handle the case when there are not enough recommendations
-            recommendations.append({
-                'title': 'Not Available',
-                'genre': 'Not Available',
-                'release_year': 'Not Available',
-                'director': 'Not Available',
-                'description': 'Not Available'
-            })
+    for match in matches:
+        title, genre, release_year, director, description = map(str.strip, match)
+        recommendations.append({
+            'title': title,
+            'genre': genre,
+            'release_year': release_year,
+            'director': director,
+            'description': description
+        })
 
     return recommendations
 
